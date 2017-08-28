f9fe8380676fb0217b059922abfa1a459d8c5bff
peijie.dpj <peijie.dpj@alibaba-inc.com>
2017-08-28 13:30:36 +0800

    fix server render

--- src/rgba-detect.js
+++ src/rgba-detect.js
@@ -2,15 +2,18 @@ let supportRGBA = true;
 
 if (typeof document !== 'undefined') {
   const scriptElement = document.getElementsByTagName('script')[0];
-  const prevColor = scriptElement.style.color;
-  try {
-    scriptElement.style.color = 'rgba(0, 0, 0, .8)';
-  } catch (e) {
-    console.error(e.stack);
-  }
 
-  supportRGBA = (scriptElement.style.color !== prevColor);
-  scriptElement.style.color = prevColor;
+  if (scriptElement) {
+    const prevColor = scriptElement.style.color;
+    try {
+      scriptElement.style.color = 'rgba(0, 0, 0, .8)';
+    } catch (e) {
+      console.error(e.stack);
+    }
+
+    supportRGBA = (scriptElement.style.color !== prevColor);
+    scriptElement.style.color = prevColor;
+  }
 }
 
 
